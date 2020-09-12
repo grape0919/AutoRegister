8b95942437074d0feebd5c1d5855744e11e2c69f
ghdry2563 <ghdry2563@gmail.com>
2020-09-12 11:26:45 +0900

    logger 추가 및 패키지 정리

--- login/AutoLogin.py
+++ login/AutoLogin.py
@@ -5,7 +5,7 @@ import requests
 import json
 import sys
 from config.Configuration import Config
-
+from log.Logger import Logger
 class AutoLogin:
 
     headers_common = {'Content-Length':'32','Accept':'*/*','X-Requested-With':'XMLHttpRequest','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36','Content-Type':'application/x-www-form-urlencoded; charset=UTF-8','Origin':'http://203.247.141.92:8080','Referer':'http://203.247.141.92:8080/SmartLogen/login','Accept-Encoding':'gzip, deflate','Accept-Language':'ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6','Connection':'close'}
@@ -15,36 +15,36 @@ class AutoLogin:
         self.config = Config()
 
     def lozenLogin(self):
-        print("lozenLogin")
+        Logger.info("lozenLogin")
         
         url = 'http://203.247.141.92:8080/SmartLogen/UserLogin'
         post = {'userid':self.config.lozenId,'userpw':self.config.lozenPwd}
         try:
             response = self.login_session.post(url,data=post,headers=self.headers_common)
         except:
-            print("lozen 로그인 중 네트워크 연결에 문제가 있습니다. ")
+            Logger.error("lozen 로그인 중 네트워크 연결에 문제가 있습니다. ")
             sys.exit()
-        print("response", response.text)
+        Logger.debug("response" + response.text)
         login_data = response.text.split('Ξ')
         self.lozenLoginData1 = login_data[1]
         self.lozenLoginData2 = login_data[3]
 
 
     def ecountLogin(self):
-        print("ecountLogin")
+        Logger.info("ecountLogin")
         url = 'https://oapi.ecounterp.com/OAPI/V2/Zone'
-        print("COM_CODE : ", self.config.ecountComCode)
+        Logger.debug("COM_CODE : " + self.config.ecountComCode)
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         post = {'COM_CODE': self.config.ecountComCode}
         
         try:
             response = requests.post(url, data=json.dumps(post), headers=headers)
         except:
-            print("ecount 로그인 중 네트워크 연결에 문제가 있습니다. ")
+            Logger.error("ecount 로그인 중 네트워크 연결에 문제가 있습니다. ")
             sys.exit()
 
-        print("response", response.json)
-        print("Data : ", response.json()["Data"]["ZONE"])
+        Logger.debug("response" + response.text)
+        Logger.debug("Data : " + response.json()["Data"]["ZONE"])
         self.ZONE = response.json()["Data"]["ZONE"]
 
         url = 'https://oapi{ZONE}.ecounterp.com/OAPI/V2/OAPILogin'.format(ZONE=self.ZONE)
@@ -55,7 +55,7 @@ class AutoLogin:
         
 
     def run(self):
-        print("run")
+        Logger.info("run")
         self.lozenLogin()
         self.ecountLogin()
 
